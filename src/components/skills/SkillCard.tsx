04381fd5ae14df5231d977e2af51b7c5202164f0
Nanangn27 <na2ng69@gmail.com>
2026-07-26 22:16:18 +0700

    Fix SkillCard JSX fragment

--- src/components/skills/SkillCard.tsx
+++ src/components/skills/SkillCard.tsx
@@ -22,6 +22,7 @@ export default function SkillCard({ skill }: SkillCardProps) {
   } = skill;
 
   return (
+<>
     
 <button
   onClick={(e) => {
@@ -72,5 +73,6 @@ export default function SkillCard({ skill }: SkillCardProps) {
         </div>
       </a>
     </Link>
-  );
+  </>
+);
 }
